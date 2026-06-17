b7d466adaa3478e90e49c98b7f8f4c3821f446d8
lkallen <lkallen.dpt@gmail.com>
2026-06-17 18:39:33 -0400

    initial components and layout for landing page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import Header from "./components/Header";
+import LandingFeatures from "./components/LandingFeatures";
+import LandingHero from "./components/LandingHero";
 
 const App = () => {
   return (
-    <div className="flex min-h-screen flex-col">
+    <div className="min-h-screen bg-background text-foreground">
       <Header />
-      <main className="flex flex-1 flex-col items-center justify-center">
-        <p className="text-muted-foreground">Coming soon!</p>
+      <main className="flex flex-1 flex-col">
+        <LandingHero />
+        <LandingFeatures />
       </main>
     </div>
   );
